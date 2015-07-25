e8b31e10bce21556529e20a6dcffa1ad3d44a381
brunoban <bruno.bandeira@gmail.com>
2015-07-25 03:09:23 -0300

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Required for Devise
+  #Note to set this for host name
+  config.action_mailer.default_url_options = { host: 'www.bandeira.me', port: 3000 }
+
 end
